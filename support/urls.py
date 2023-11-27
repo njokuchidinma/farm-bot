79c463695ed01201a7767cae90ea365ff15db4e0
njokuchidinma <njokuchidinma16@gmail.com>
2023-11-27 23:34:07 +0100

    statics

--- support/urls.py
+++ support/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -41,4 +43,4 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('redoc/', schema_view.with_ui('redoc'), name="swagger"),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
